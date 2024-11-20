1656534a24a8f28d9c88ef503bc59c8e097b868f
Muhammad Attiq Zahoor <162964441+attiqzahoor@users.noreply.github.com>
2024-11-20 12:57:25 +0500

    Create LionTiger.sol

---
+++ LionTiger.sol
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+// Chainlink Imports
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+// This import includes functions from both ./KeeperBase.sol and
+// ./interfaces/KeeperCompatibleInterface.sol
+import "@chainlink/contracts/src/v0.8/KeeperCompatible.sol";
+
+// Dev imports. This only works on a local dev network
+// and will not work on any test or main livenets.
+import "hardhat/console.sol";
+
+contract LionTiger is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    // IPFS URIs for the dynamic nft graphics/metadata.
+    // NOTE: These connect to my IPFS Companion node.
+    // You should upload the contents of the /ipfs folder to your own node for development.
+    string[] lionUrisIpfs = [
+        "https://ipfs.io/ipfs/QmRXyfi3oNZCubDxiVFre3kLZ8XeGt6pQsnAQRZ7akhSNs?filename=lion_king.json",
+        "https://ipfs.io/ipfs/QmRJVFeMrtYS2CUVUM2cHJpBV5aX2xurpnsfZxLTTQbiD3?filename=roaring_lion.json",
+        "https://ipfs.io/ipfs/QmdcURmN1kEEtKgnbkVJJ8hrmsSWHpZvLkRgsKKoiWvW9g?filename=savanna_lion.json"
+    ];
+    string[] tigerUrisIpfs = [
+        "https://ipfs.io/ipfs/Qmdx9Hx7FCDZGExyjLR6vYcnutUR8KhBZBnZfAPHiUommN?filename=siberian_tiger.json",
+        "https://ipfs.io/ipfs/QmTVLyTSuiKGUEmb88BgXG3qNC8YgpHZiFbjHrXKH3QHEu?filename=royal_tiger.json",
+        "https://ipfs.io/ipfs/QmbKhBXVWmwrYsTPFYfroR2N7NAekAMxHUVg2CWks7i9qj?filename=stripe_tiger.json"
+    ];
+
+    constructor() ERC721("Lion&Tiger", "LTNFT") {}
+
+    function safeMint(address to) public {
+        // Current counter value will be the minted token's token ID.
+        uint256 tokenId = _tokenIdCounter.current();
+
+        // Increment it so next time it's correct when we call .current()
+        _tokenIdCounter.increment();
+
+        // Mint the token
+        _safeMint(to, tokenId);
+
+        // Default to a lion NFT
+        string memory defaultUri = lionUrisIpfs[0];
+        _setTokenURI(tokenId, defaultUri);
+
+        console.log(
+            "DONE!!! minted token ",
+            tokenId,
+            " and assigned token url: ",
+            defaultUri
+        );
+    }
+
+    // The following functions are overrides required by Solidity.
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721, ERC721URIStorage)
+    {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
